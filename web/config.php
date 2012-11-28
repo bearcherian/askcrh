afc91902b7695b9116a541fc218505bb8a6a2ca3
Bear Cherian <bearcherian@gmail.com>
2012-11-28 22:07:35 +0000

    adding files/code to sign in to the application with twitter
    git-svn-id: https://svn.code.sf.net/p/askcrh/code/trunk@27 889e6e00-b29b-4d12-9244-fab03fbf201a

---
+++ web/config.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file
+ * A single location to store configuration.
+ */
+
+define('CONSUMER_KEY', 'v2uncgaHPPWupobjcemw');
+define('CONSUMER_SECRET', 'Gu9fhLCeKKJdxHRBzZnfrYu1mD8FJF2aibacqybuA8');
+define('OAUTH_CALLBACK', 'http://askcrh.indianbear.com/web/callback.php');
+
+?>
