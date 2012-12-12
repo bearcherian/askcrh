25048ddd3e31853ce12f88e6861cb364efbaf13c
Bear Cherian <bearcherian@gmail.com>
2012-12-12 16:53:14 +0000

    Fixed references to class variables and other items.
    git-svn-id: https://svn.code.sf.net/p/askcrh/code/trunk@45 889e6e00-b29b-4d12-9244-fab03fbf201a

--- data/Members.php
+++ data/Members.php
@@ -2,49 +2,50 @@
 require_once('DBConnect.php');
 
 class Members {
-	private $db;
 	private $members; //$members[id]
 
 	public function __construct() {
-		$db = new DBConnect();
-		$db->connect();
-		$results = $db->query('SELECT * FROM members');
+		$this->members = array();
+		$results = mysql_query('SELECT * FROM members');
 		$num = mysql_num_rows($results);
-		echo "Member results: " . $num;
+	//	echo "Member results: " . $num;
 		for ( $i = 0; $i < $num; $i++) {
 			$id = mysql_result($results,$i,"id");
 			$handle = mysql_result($results, $i, "handle"); 
-			$members[$id] = $handle;		}
+		//	echo $id . ":" . $handle . "<br />";
+			$this->members[$id] = $handle;
+		}
 	}
 
 	public function getMembers() {
-		return $members;
+	//	echo "MEMBERS - Members dump";
+	//	print_r($members);
+		return $this->members;
 	}
 
 	public function addMember($id, $handle) {
 		
 		//Check if this ID already exists
-		$db->connect();
-		$mm = $db->query('SELECT * from members where id = "' . $id . '"');
+		$mm = mysql_query('SELECT * from members where id = "' . $id . '"');
 		if (mysql_num_rows($mm) > 0) return false;
 		
 		//Add this member to DB and to the members array
-		$amq = $this->db->query('INSERT into members(id,handle) VALUES ("' . $id . '","' . $handle . '")');
-		$this->$members[$id] = $handle;
+		$amq = mysql_query('INSERT into members(id,handle) VALUES ("' . $id . '","' . $handle . '")');
+		$this->members[$id] = $handle;
 
 	}
 
 	public function getHandle($id) {
-		return $this->$members[$id];
+		return $this->members[$id];
 	}
 
 	public function getId($handle){
-		return array_search($this->$members, $handle);
+		return array_search($this->members, $handle);
 	}
 
 	public function removeMember($id) {
-		$this->$db->query('DELETE FROM members WHERE id = "' . $id . '"');
-		unset($this->$members[$id]);
+		mysql_query('DELETE FROM members WHERE id = "' . $id . '"');
+		unset($this->members[$id]);
 	}
 }
 ?>
--- data/Topics.php
+++ data/Topics.php
@@ -4,55 +4,61 @@ require_once('DBConnect.php');
 
 class Topics {
 
-	private $db;
 	private $topics;
 
 	public function __construct(){
-		$db = new DBConnect();
 		$this->loadTopics();
 	}
 
 	private function loadTopics() {
-		$topics = null;
-		$results = $db->query('SELECT DISTINCT topic FROM topics');
+		$this->topics = array();
+		$results = mysql_query('SELECT DISTINCT topic FROM topics');
 		$num = mysql_num_rows($results);
 		for ($i = 0; $i < $num; $i++) {
-			$topics[$i] = mysql_result($results,$i,"topic");
+			$this->topics[$i] = mysql_result($results,$i,"topic");
 		}
-		echo "...topics loaded...";
 	}
 
 	public function getTopics() {
-		return $this->$topics;
+		return $this->topics;
 	}
 
 	public function addTopicMember ($topic, $member) {
-		$results = $this->$db->query('SELECT * FROM topics WHERE topic = "' . $topic . '" AND member = "' . $member . '"');
-		if (mysql_num_rows(results) > 0 ) return true;
+		$results = mysql_query('SELECT * FROM topics WHERE topic = "' . $topic . '" AND member = "' . $member . '"');
+		if (mysql_num_rows($results) > 0 ) return true;
 		
-		$this->$db->query('INSERT INTO topics (topic, member) VALUES ("' . $topic . '", "' . $member . '")');
-		array_push($this->$topics,$topic);
+		mysql_query('INSERT INTO topics (topic, member) VALUES ("' . $topic . '", "' . $member . '")');
+		array_push($this->topics,$topic);
 	}
 
 	public function removeTopicMember ($topic, $member) {
-		$this->$db->query('DELETE FROM topics WHERE topic = "' . $topic . '" AND member = "' . $member . '"');
+		mysql_query('DELETE FROM topics WHERE topic = "' . $topic . '" AND member = "' . $member . '"');
 		$this->loadTopics();
 	}
 
 	public function removeAllMemberTopics ($member) {
-		$this->$db->query('DELETE FROM topics WHERE member = "' . $member . '"');
+		mysql_query('DELETE FROM topics WHERE member = "' . $member . '"');
 		$this->loadTopics();
 
 	}
 
 	public function getMembersForTopic($topic) {
-		$memberIds = null;
-		$results = $this->$db->query('SELECT DISTINCT member FROM topics WHERE topic = "' . $topic . '"');
-		$num = $mysql_num_rows($results);
+		$memberIds = array();
+		$results = mysql_query('SELECT DISTINCT member FROM topics WHERE topic = "' . $topic . '"');
+		$num = mysql_num_rows($results);
 		for ($i = 0; $i < $num; $i++) {
 			$memberIds[$i] = mysql_result($results,$i,"member");
 		}
 		return $memberIds;
+	}
+	
+	public function getTopicsForMember($id) {
+		$results = mysql_query('SELECT topic FROM topics WHERE member = "' . $id . '"');
+		$num = mysql_num_rows($results);
+		for ($i = 0; $i < $num; $i++) {
+			$mtop[$i] = mysql_result($results,$i,"topic");
+		}
+		return $mtop;
 	}
 }
 
