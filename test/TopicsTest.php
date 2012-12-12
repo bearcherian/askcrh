35baf983fe2f89bf5e05b33049d688a237703bc6
Bear Cherian <bearcherian@gmail.com>
2012-12-12 16:53:42 +0000

    Updated TEST for topics
    git-svn-id: https://svn.code.sf.net/p/askcrh/code/trunk@46 889e6e00-b29b-4d12-9244-fab03fbf201a

--- test/TopicsTest.php
+++ test/TopicsTest.php
@@ -1,17 +1,29 @@
 <?
 require_once('../data/Topics.php');
 
+echo "Creating Topics object...<br />";
 $topics = new Topics();
 
-echo "Adding topic science for member 1234...";
+echo "Topics:<pre>";
+var_dump($topics->getTopics());
+echo "</pre><br />";
+
+echo "Adding topic science for member 1234...<br />";
 $topics->addTopicMember("science","1234");
 
-echo "Retrieving list of topics...";
+echo "Topics:<pre>";
 var_dump($topics->getTopics());
+echo "</pre><br />";
 
-echo "Displaying members for topic science...";
+echo "Displaying members for topic science...<br /><pre>";
 var_dump($topics->getMembersForTopic("science"));
+echo "</pre><br/>";
 
-echo "Deleting Topics...";
+echo "Deleting Topic science for member 1234...<br />";
 $topics->removeTopicMember("science","1234");
+
+echo "Topics:<pre>";
+var_dump($topics->getTopics());
+echo "</pre><br />";
+
 ?>
