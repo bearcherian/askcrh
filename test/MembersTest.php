77bdd98a436df4c9581aad0dd1b426196f7531a4
Bear Cherian <bearcherian@gmail.com>
2012-12-12 15:21:36 +0000

    updated Members unit test
    git-svn-id: https://svn.code.sf.net/p/askcrh/code/trunk@43 889e6e00-b29b-4d12-9244-fab03fbf201a

--- test/MembersTest.php
+++ test/MembersTest.php
@@ -1,10 +1,26 @@
 <?
 require_once('../data/Members.php');
 
+echo "Creating Members object...<br />";
 $members = new Members();
 
+echo "Members: <br /><pre>";
 print_r($members->getMembers());
+echo "</pre><br />";
 
+echo "Adding member \"fake\" with id 1234...<br />";
 $members->addMember("1234","fake");
+
+echo "Members: <br /><pre>";
+print_r($members->getMembers());
+echo "</pre><br />";
+
+echo "Removing member 1234...<br />";
+$members->removeMember("1234");
+
+echo "Members: <br /><pre>";
+print_r($members->getMembers());
+echo "</pre><br />";
+
 ?>
 
